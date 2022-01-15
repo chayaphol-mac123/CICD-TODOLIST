2715fafaf5c6aa828d245cb0165987d4869e0d8d
macchayaphol <55339550+macchayaphol@users.noreply.github.com>
2022-01-15 12:44:20 +0700

    finish dashboard 6210742026

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,46 +6,112 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../public/css/dashboard.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
     <style>
         .add-new-task-btn {
-            background-color: dodgerblue;
-            color: #fff;
+            background-color: #fff;
+            color: #9932CC;
             padding: 10px 20px;
             border-radius: 10px;
             transition: 0.3s;
             font-weight: bold;
         }
         .add-new-task-btn:hover {
-            background-color: blue;
+            background-color: #9932CC;
+            color: #fff;
+            border: 2px solid #fff;
         }
         .tr-todolist {
             transition: 0.3s;
             border-radius: 10px;
-            border-top: 0.5px solid #C0C0C0;
-            border-bottom: 0.5px solid #C0C0C0;
-            background-size: cover;
+            
+            font-weight: bold;
+            color:#9932CC;
+            background-color: #fff;
+            overflow: hidden;
+            border-radius: 10px !important;
+
         }
         .tr-todolist:hover{
             background-color: #DCDCDC;
+            
         }
         .task-list-header{
             font-weight: bold;
             font-size: 24px;
             transition: .1s;
+            color: #fff;
         }
         .task-list-header:hover {
             font-size: 32px;
         }
         .container {
-            background-color: #fff;
+            
+            background: -webkit-linear-gradient(#9932CC, #87CEFA); 
+            background: -o-linear-gradient(#9932CC, #87CEFA);
+            background: -moz-linear-gradient(#9932CC, #87CEFA); 
+            background: linear-gradient(#9932CC, #87CEFA); 
+
             overflow: hidden;
             border-radius: 10px;
             padding: 30px;
-            border: 3px solid purple;
+            
+        }
+        #task1 {
+            color: #fff;
+            
+
+        }
+        #action1 {
+            color: #fff;
+            
+        }
+        
+        td:first-child {
+            border-top-left-radius: 20px; 
+            border-bottom-left-radius: 20px;
+        }
+        td:last-child {
+            border-bottom-right-radius: 20px; 
+            border-top-right-radius: 20px; 
         }
 
+        #spaceing-table {
+            border-collapse: separate;
+            table-layout: fixed;
+            border-spacing: 0 5px; /* this is the ultimate fix */
+        }
+        #spaceing-table th {
+            text-align: left;
+            
+        }
+        #spaceing-table td {
+            border-width: 3px 0;
+            
+        }
+        #spaceing-table thead {
+            display: table;
+            table-layout: fixed;
+            width: 100%;
+        }
+        #spaceing-table tbody {
+            display: table;
+            table-layout: fixed;
+            width: 100%;
+            border-spacing: 0 10px;
+        }
+        /* #edit-task:hover {
+            transform: scale(1.5) perspective(10px);
+            transition: 0.1s;
+        }
+        #edit-task:hover {
+            transition: 0.1s;
+        } */
+
         
+       
+
      
     </style>
 
@@ -67,29 +133,29 @@
                     <a href="/task" class="add-new-task-btn">+  Add new Task</a>
                 </div>
             </div>
-            <table class="w-full text-md rounded mb-4">
+            <table class="w-full text-md rounded mb-4" id="spaceing-table">
                 <thead>
                 <tr class="tr-task-action">
-                    <th class="text-left p-3 px-5">Task</th>
-                    <th class="text-left p-3 px-5">Actions</th>
-                    <th></th>
+                    <th class="text-left p-3 px-5" id="task1">Task</th>
+                    <th class="text-left p-3 px-5" id="action1">Actions</th>
                 </tr>
                 </thead>
                 <tbody>
                 @foreach(auth()->user()->tasks as $task)
                     <tr class="tr-todolist">
-                        <td class="p-3 px-5">
+                        <td class="p-3 px-5" id="list-todo" >
                             {{$task->description}}
                         </td>
                         <td class="p-3 px-5">
                             
-                            <a href="/task/{{$task->id}}" name="edit" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Edit</a>
+                            <a href="/task/{{$task->id}}" name="edit" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline" id="edit-task"><i class="fas fa-edit"></i></a>
                             <form action="/task/{{$task->id}}" class="inline-block">
-                                <button type="submit" name="delete" formmethod="POST" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Delete</button>
+                                <button type="submit" name="delete" formmethod="POST" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"><i class="fas fa-trash"></i></button>
                                 {{ csrf_field() }}
                             </form>
                         </td>
                     </tr>
+                    
                 @endforeach
                 </tbody>
             </table>
