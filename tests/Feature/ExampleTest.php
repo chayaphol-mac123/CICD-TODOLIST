cf4e9758a9e5573c4b116b32eb1a1d16cb878c83
macchayaphol <55339550+macchayaphol@users.noreply.github.com>
2022-03-16 11:54:25 +0700

    test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,31 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use WithFaker;
 
-        $response->assertStatus(200);
+    public function testUserCreationEndpointTest()
+    {
+        $name = $this->faker->name();
+        $email = $this->faker->email();
+        $password = "mypassword";
+
+        $response = $this->postJson('/api/createuser', [
+            'name' => $name, 
+            'email' => $email,
+            'password' => $password,
+            'password_confirmation' => $password
+        ]); 
+
+        $response
+            ->assertStatus(201)
+            ->assertExactJson([
+                'message' => "Successfully created user!",
+            ]);
     }
 }
